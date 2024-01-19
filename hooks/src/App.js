5d5a9376c9decdaa89e52330b81d256c3c21e485
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-01-19 18:42:51 +0600

    up

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import { ProductList } from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>Product List</h1>
+      <ProductList />
     </div>
   );
 }
